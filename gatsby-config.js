3acaa41fe1bf4dc728c6e154f54146a7c2c7b4eb
Khushwant Singh <khsingh@adobe.com>
2021-11-30 23:09:48 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,5 +152,5 @@ module.exports = {
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/experience-manager-forms-cloud-service-developer-reference/'
+  pathPrefix: process.env.PATH_PREFIX || '/experience-manager-forms-cloud-service/'
 };
